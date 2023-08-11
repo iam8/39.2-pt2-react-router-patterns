408887eba8317e57d50dfbf506187db2a6fe10bb
alexm <alex-m@outlook.com>
2023-08-11 13:25:59 -0700

    Add color list prop; render list of colors and functional links

--- src/ColorList.js
+++ src/ColorList.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import formatColorName from "./helpers/formatColorName";
 import "./ColorList.css";
 
 
-function ColorList () {
+function ColorList ({colors}) {
 
     return (
         <div className="ColorList">
-            Color List here
+            <ul>
+                {
+                    colors.map((color, ind) => {
+                        return (
+                            <li key={ind}>
+                                <Link to={`/colors/${formatColorName(color)}`}>
+                                    {color}
+                                </Link>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
         </div>
     )
 }
