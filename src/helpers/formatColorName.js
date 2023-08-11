248bd1c3e3a8b5709a2c6da1e23048c7c6586c3d
alexm <alex-m@outlook.com>
2023-08-11 13:24:53 -0700

    Create helper function that formats color names to be CSS and link-friendly

---
+++ src/helpers/formatColorName.js
@@ -0,0 +1,14 @@
+
+/**
+ * Format a given color name to be usable in CSS and links.
+ *
+ * Specifics: lowercase the color name and remove all spaces (if any).
+ *
+ * Return the formatted color name (string).
+ */
+function formatColorName(color) {
+    return color.toLowerCase().replace(" ", "");
+}
+
+
+export default formatColorName;
