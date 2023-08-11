de2aca05cf9ec19b3aa383a0adf6387488574190
alexm <alex-m@outlook.com>
2023-08-10 22:47:03 -0700

    Import and render appropriate components for each route

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,24 @@
 import React from "react";
 import {Route, Switch, Redirect} from "react-router-dom";
 
+import HomePage from "./HomePage";
+import NewColorForm from "./NewColorForm";
+import ColorDetails from "./ColorDetails";
+
 
 function Routes () {
     return (
         <Switch>
             <Route exact to="/colors">
-
+                <HomePage />
             </Route>
 
             <Route exact to="/colors/new">
-
+                <NewColorForm />
             </Route>
 
             <Route exact to="/colors/:color">
-
+                <ColorDetails />
             </Route>
 
             <Redirect to="/colors"/>
