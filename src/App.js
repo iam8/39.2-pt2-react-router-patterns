5243b1003623077c527697df75616628372c865a
alexm <alex-m@outlook.com>
2023-08-10 22:36:49 -0700

    Add imports and render routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './Routes';
 import './App.css';
 
 
 function App() {
     return (
         <div className="App">
-
+            <BrowserRouter>
+                <Routes />
+            </BrowserRouter>
         </div>
     );
 }
