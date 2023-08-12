3f19069bf091de80a79b56e8c57d058254098a77
alexm <alex-m@outlook.com>
2023-08-12 12:26:54 -0700

    Wrap Link in div and add class name

--- src/AddColorBanner.js
+++ src/AddColorBanner.js
@@ -11,9 +11,11 @@ function AddColorBanner () {
 
             <h1 className="AddColorBanner-heading">Welcome to the Color Factory.</h1>
 
-            <Link to="/colors/new">
-                Add a color
-            </Link>
+            <div className="AddColorBanner-addlink">
+                <Link to="/colors/new">
+                    Add a color
+                </Link>
+            </div>
 
         </div>
     )
